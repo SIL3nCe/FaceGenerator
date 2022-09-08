5e2c2981c98184540719e4f74ec8cee1e6035d74
SIL3nCe <tom.pradal@aliceadsl.fr>
2022-09-09 00:33:14 +0200

    Added rand name text generation

--- FaceGenerator_Project/Assets/Generator/FaceGenerator.cs
+++ FaceGenerator_Project/Assets/Generator/FaceGenerator.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using TMPro;
+
+public struct SName
+{
+    public string FirstName;
+    public string LastName;
+}
 
 public class FaceGenerator : MonoBehaviour
 {
     public int BaseImageHeight = 512;
 
+    //TODO dyn array of struct for this
     public SpriteRenderer FaceUpRenderer;
     public int FaceUpPixelHeight = 150;
 
@@ -21,6 +29,10 @@ public class FaceGenerator : MonoBehaviour
 
     private List<Texture2D> _imageList = new();
 
+    private List<SName> _namesList = new();
+
+    public TMP_Text NameText;
+
     void Start()
     {
         if (Directory.Exists("./Images/"))
@@ -29,6 +41,15 @@ public class FaceGenerator : MonoBehaviour
             foreach (FileInfo file in dirInfo.GetFiles())
             {
                 CreateTextureFromFilePath(file.FullName);
+
+                string[] nameSplit = file.Name.Split("_");
+                if (nameSplit.Length >= 1)
+                {
+                    SName name = new();
+                    name.FirstName = nameSplit[0];
+                    name.LastName = nameSplit[1];
+                    _namesList.Add(name);
+                }
             }
 
             GenerateFace();
@@ -58,37 +79,63 @@ public class FaceGenerator : MonoBehaviour
         if (_imageList.Count == 0)
             return;
 
+        List<int> faceIds = new();
+
         int pixelHeightAccumulation = FaceUpPixelHeight;
 
         // Up
-        Texture2D baseTexture = _imageList[Random.Range(0, _imageList.Count)];
+        int faceId = Random.Range(0, _imageList.Count);
+        faceIds.Add(faceId);
+
+        Texture2D baseTexture = _imageList[faceId];
         Texture2D texture_up = new(baseTexture.width, FaceUpPixelHeight);
         texture_up.SetPixels(baseTexture.GetPixels(0, baseTexture.height - pixelHeightAccumulation, baseTexture.width, FaceUpPixelHeight));
         texture_up.Apply();
         FaceUpRenderer.sprite = Sprite.Create(texture_up, new Rect(0.0f, 0.0f, texture_up.width, texture_up.height), new Vector2(0.5f, 0.5f));
 
         // Eyes
+        faceId = Random.Range(0, _imageList.Count);
+        faceIds.Add(faceId);
+
         pixelHeightAccumulation += FaceEyesPixelHeight;
-        baseTexture = _imageList[Random.Range(0, _imageList.Count)];
+        baseTexture = _imageList[faceId];
         Texture2D texture_eyes = new(baseTexture.width, FaceEyesPixelHeight);
         texture_eyes.SetPixels(baseTexture.GetPixels(0, baseTexture.height - pixelHeightAccumulation, baseTexture.width, FaceEyesPixelHeight));
         texture_eyes.Apply();
         FaceEyesRenderer.sprite = Sprite.Create(texture_eyes, new Rect(0.0f, 0.0f, texture_eyes.width, texture_eyes.height), new Vector2(0.5f, 0.5f));
 
         // Nose/Mouth
+        faceId = Random.Range(0, _imageList.Count);
+        faceIds.Add(faceId);
+
         pixelHeightAccumulation += FaceNosePixelHeight;
-        baseTexture = _imageList[Random.Range(0, _imageList.Count)];
+        baseTexture = _imageList[faceId];
         Texture2D texture_nose = new(baseTexture.width, FaceNosePixelHeight);
         texture_nose.SetPixels(baseTexture.GetPixels(0, baseTexture.height - pixelHeightAccumulation, baseTexture.width, FaceNosePixelHeight));
         texture_nose.Apply();
         FaceNoseRenderer.sprite = Sprite.Create(texture_nose, new Rect(0.0f, 0.0f, texture_nose.width, texture_nose.height), new Vector2(0.5f, 0.5f));
 
         // Bottom
-        baseTexture = _imageList[Random.Range(0, _imageList.Count)];
+        faceId = Random.Range(0, _imageList.Count);
+        faceIds.Add(faceId);
+
+        baseTexture = _imageList[faceId];
         Texture2D texture_bottom = new(baseTexture.width, FaceBottomPixelHeight);
         texture_bottom.SetPixels(baseTexture.GetPixels(0, 0, baseTexture.width, FaceBottomPixelHeight));
         texture_bottom.Apply();
         FaceBottomRenderer.sprite = Sprite.Create(texture_bottom, new Rect(0.0f, 0.0f, texture_bottom.width, texture_bottom.height), new Vector2(0.5f, 0.5f));
+
+        // Get random name in the selected faces and invert first letters
+        if (_namesList.Count == _imageList.Count)
+        {
+            SName randName = new();
+            string firstName = _namesList[Random.Range(0, _namesList.Count)].FirstName;
+            string lastName = _namesList[Random.Range(0, _namesList.Count)].LastName;
+            randName.FirstName = lastName[0].ToString().ToUpper() + firstName.Remove(0, 1);
+            randName.LastName = firstName[0].ToString().ToUpper() + lastName.Remove(0, 1);
+
+            NameText.text = randName.FirstName+ " " + randName.LastName;
+        }
     }
 
     private void OnValidate()
